7f4d4fdb74dbb600f632a5a2c4b6d7e8d762d872
ziankork <zian@cork.technology>
2024-10-16 15:47:38 +0800

    feat: added add and remove liquidity in libs

--- src/lib/State.sol
+++ src/lib/State.sol
@@ -1,26 +1,80 @@
 pragma solidity ^0.8.0;
 
+import "./LiquidityMath.sol";
+import "./../LiquidityToken.sol";
+import "v4-periphery/lib/v4-core/src/types/Currency.sol";
 
-/// @notice amm id, must be the same as pool id in cork
+/// @notice amm id,
 type AmmId is bytes32;
 
+function toAmmId(address ra, address ct) pure returns (AmmId) {
+    (address token0, address token1) = sort(ra, ct);
+
+    return AmmId.wrap(keccak256(abi.encodePacked(token0, token1)));
+}
+
+function sort(address a, address b) pure returns (address, address) {
+    return a < b ? (a, b) : (b, a);
+}
+
+function sort(address a, address b, uint256 amountA, uint256 amountB)
+    pure
+    returns (address, address, uint256, uint256)
+{
+    return a < b ? (a, b, amountA, amountB) : (b, a, amountB, amountA);
+}
+
 struct PoolState {
-    uint256 reserveRA;
-    uint256 reserveCT;
-    address ra;
-    address ct;
+    uint256 reserve0;
+    uint256 reserve1;
+    address token0;
+    address token1;
     // should be deployed using clones
-    address liquidityToken;
+    LiquidityToken liquidityToken;
 }
 
 library PoolStateLibrary {
-    function initialize(PoolState storage state, address _ra, address _ct, address _liquidityToken) internal {
-        state.ra = _ra;
-        state.ct = _ct;
-        state.liquidityToken = _liquidityToken;
+    function getToken0(PoolState storage state) internal view returns (Currency) {
+        return Currency.wrap(state.token0);
+    }
+
+    function getToken1(PoolState storage state) internal view returns (Currency) {
+        return Currency.wrap(state.token1);
+    }
+
+    function initialize(PoolState storage state, address _token0, address _token1, address _liquidityToken) internal {
+        state.token0 = _token0;
+        state.token1 = _token1;
+        state.liquidityToken = LiquidityToken(_liquidityToken);
     }
 
     function isInitialized(PoolState storage state) internal view returns (bool) {
-        return state.ra != address(0);
+        return state.token0 != address(0);
+    }
+
+    function addLiquidity(PoolState storage state, uint256 amount0, uint256 amount1, address sender)
+        internal
+        returns (uint256 reserve0, uint256 reserve1, uint256 mintedLp)
+    {
+        (reserve0, reserve1, mintedLp) = LiquidityMath.addLiquidity(
+            state.reserve0, state.reserve1, state.liquidityToken.totalSupply(), amount0, amount1
+        );
+
+        state.reserve0 = reserve0;
+        state.reserve1 = reserve1;
+        state.liquidityToken.mint(sender, mintedLp);
+    }
+
+    function removeLiquidity(PoolState storage state, uint256 liquidityAmount, address sender)
+        internal
+        returns (uint256 amount0, uint256 amount1, uint256 reserve0, uint256 reserve1)
+    {
+        (amount0, amount1, reserve0, reserve1) = LiquidityMath.removeLiquidity(
+            state.reserve0, state.reserve1, state.liquidityToken.totalSupply(), liquidityAmount
+        );
+
+        state.reserve0 = reserve0;
+        state.reserve1 = reserve1;
+        state.liquidityToken.burnFrom(sender, liquidityAmount);
     }
 }
