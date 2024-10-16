ae11735e3709db43820ce4c0e6e4e205d2b10eeb
ziankork <zian@cork.technology>
2024-10-16 15:47:18 +0800

    chore: rename things

--- src/lib/LiquidityMath.sol
+++ src/lib/LiquidityMath.sol
@@ -2,62 +2,69 @@
 pragma solidity ^0.8.0;
 
 library LiquidityMath {
-
     // Adding Liquidity (Pure Function)
     function addLiquidity(
-        uint256 reserveRA, // Current reserve of RA (target token)
-        uint256 reserveCT, // Current reserve of CT (yield-bearing token)
+        uint256 reserve0, // Current reserve of RA (target token)
+        uint256 reserve1, // Current reserve of CT (yield-bearing token)
         uint256 totalLiquidity, // Total current liquidity (LP token supply)
-        uint256 amountRA, // Amount of RA to add
-        uint256 amountCT // Amount of CT to add
-    ) internal pure returns (
-        uint256 newReserveRA, // Updated reserve of RA
-        uint256 newReserveCT, // Updated reserve of CT
-        uint256 liquidityMinted // Amount of liquidity tokens minted
-    ) {
+        uint256 amount0, // Amount of RA to add
+        uint256 amount1 // Amount of CT to add
+    )
+        internal
+        pure
+        returns (
+            uint256 newReserveRA, // Updated reserve of RA
+            uint256 newReserveCT, // Updated reserve of CT
+            uint256 liquidityMinted // Amount of liquidity tokens minted
+        )
+    {
         // Ensure the added amounts are proportional
-        require(amountRA * reserveCT == amountCT * reserveRA, "Non-proportional liquidity");
+        require(amount0 * reserve1 == amount1 * reserve0, "Non-proportional liquidity");
 
         // Calculate the liquidity tokens minted based on the added amounts and the current reserves
         if (totalLiquidity == 0) {
             // Initial liquidity provision (sqrt of product of amounts added)
-            liquidityMinted = sqrt(amountRA * amountCT);
+            liquidityMinted = sqrt(amount0 * amount1);
         } else {
             // Mint liquidity proportional to the added amounts
-            liquidityMinted = (amountRA * totalLiquidity) / reserveRA;
+            liquidityMinted = (amount0 * totalLiquidity) / reserve0;
         }
 
         // Update reserves
-        newReserveRA = reserveRA + amountRA;
-        newReserveCT = reserveCT + amountCT;
+        newReserveRA = reserve0 + amount0;
+        newReserveCT = reserve1 + amount1;
 
         return (newReserveRA, newReserveCT, liquidityMinted);
     }
 
     // Removing Liquidity (Pure Function)
     function removeLiquidity(
-        uint256 reserveRA, // Current reserve of RA (target token)
-        uint256 reserveCT, // Current reserve of CT (yield-bearing token)
+        uint256 reserve0, // Current reserve of RA (target token)
+        uint256 reserve1, // Current reserve of CT (yield-bearing token)
         uint256 totalLiquidity, // Total current liquidity (LP token supply)
         uint256 liquidityAmount // Amount of liquidity tokens being removed
-    ) internal pure returns (
-        uint256 amountRA, // Amount of RA returned to the LP
-        uint256 amountCT, // Amount of CT returned to the LP
-        uint256 newReserveRA, // Updated reserve of RA
-        uint256 newReserveCT // Updated reserve of CT
-    ) {
+    )
+        internal
+        pure
+        returns (
+            uint256 amount0, // Amount of RA returned to the LP
+            uint256 amount1, // Amount of CT returned to the LP
+            uint256 newReserveRA, // Updated reserve of RA
+            uint256 newReserveCT // Updated reserve of CT
+        )
+    {
         require(liquidityAmount > 0, "Invalid liquidity amount");
         require(totalLiquidity > 0, "No liquidity available");
 
         // Calculate the proportion of reserves to return based on the liquidity removed
-        amountRA = (liquidityAmount * reserveRA) / totalLiquidity;
-        amountCT = (liquidityAmount * reserveCT) / totalLiquidity;
+        amount0 = (liquidityAmount * reserve0) / totalLiquidity;
+        amount1 = (liquidityAmount * reserve1) / totalLiquidity;
 
         // Update reserves after removing liquidity
-        newReserveRA = reserveRA - amountRA;
-        newReserveCT = reserveCT - amountCT;
+        newReserveRA = reserve0 - amount0;
+        newReserveCT = reserve1 - amount1;
 
-        return (amountRA, amountCT, newReserveRA, newReserveCT);
+        return (amount0, amount1, newReserveRA, newReserveCT);
     }
 
     // Helper function to calculate square root
