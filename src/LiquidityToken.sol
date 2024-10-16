5d67b296d1c5423815cf3be6f3723e08b6603649
ziankork <zian@cork.technology>
2024-10-16 13:01:02 +0800

    chore: deps

---
+++ src/LiquidityToken.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+
+contract LiquidityToken is Initializable {}
--- src/LpClone.sol
+++
@@ -1,4 +0,0 @@
-pragma solidity ^0.8.0;
-import openzppelerc20
-
-contract LiquidityToken {}
