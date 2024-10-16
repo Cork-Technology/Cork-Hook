36777a50af436a1b4e186962dadb6777ca1a3a49
ziankork <zian@cork.technology>
2024-10-16 15:49:41 +0800

    feat: add calls and callback params

---
+++ src/lib/Calls.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+enum Action {
+    AddLiquidity,
+    RemoveLiquidity,
+    Swap
+}
+
+struct AddLiquidtyParams {
+    address token0;
+    uint256 amount0;
+    address token1;
+    uint256 amount1;
+    address sender;
+}
+
+struct RemoveLiquidtyParams {
+    address token0;
+    address token1;
+    uint256 liquidityAmount;
+    address sender;
+}
