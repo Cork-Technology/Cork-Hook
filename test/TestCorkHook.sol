654912ea66eac730dca69b820c445dcfb6ca92cf
ziankork <zian@cork.technology>
2024-10-17 20:14:59 +0800

    test: add test helper hook

--- test/TestCorkHook.sol
+++ test/TestCorkHook.sol
@@ -1,12 +1,13 @@
 pragma solidity ^0.8.0;
 
 import {CorkHook, LiquidityToken, AmmId, PoolState} from "./../src/CorkHook.sol";
+import {toAmmId} from "./../src/lib/State.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 
 contract TestCorkHook is CorkHook {
     constructor(IPoolManager _poolManager, LiquidityToken _lpBase) CorkHook(_poolManager, _lpBase) {}
 
-    function getPoolState(AmmId ammId) public view returns (PoolState memory) {
-        return pool[ammId];
+    function getPoolState(address tokenA, address tokenB) public view returns (PoolState memory) {
+        return pool[toAmmId(tokenA, tokenB)];
     }
 }
